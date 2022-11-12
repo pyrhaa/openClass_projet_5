716fbffd1794984efd3e4d0340922cf114213991
pyrhaa <sboubtane@protonmail.com>
2022-11-12 16:37:07 +0100

    products js not finished, code error, see tomorrow

--- front/js/products.js
+++ front/js/products.js
@@ -13,6 +13,7 @@ const selectQuantity = document.querySelector('#quantity');
 // select button add to cart
 const addToCart = document.querySelector('#addToCart');
 
+//function 1
 const selectedProduct = (product) => {
   // Integrate product data to the page HTML
   document.querySelector('head > title').textContent = product.name;
@@ -30,6 +31,49 @@ const selectedProduct = (product) => {
     option.value = `${color}`;
     selectColor.appendChild(option);
   }
+};
+
+//function 2
+const registerProduct = (product) => {
+  addToCart.addEventListener('click', (event) => {
+    event.preventDefault();
+    const cartData = JSON.parse(localStorage.getItem('cart'));
+    const selectedProducts = {
+      id: product._id,
+      name: product.name,
+      img: product.imageUrl,
+      altTxt: product.altTxt,
+      description: product.description,
+      color: selectColor.value,
+      quantity: parseInt(selectQuantity.value, 10)
+    };
+
+    console.log(selectedProducts);
+
+    if (cartData) {
+      console.log('Il y a déjà un produit dans le panier');
+      let itemArray = cartData.find(
+        (item) =>
+          item.id == selectedProducts.id && item.color == selectedProducts.color
+      );
+      if (itemArray) {
+        itemArray.quantity = itemArray.quantity + selectedProducts.quantity;
+        itemArray.totalPrice += itemArray.price * selectedProducts.quantity;
+        localStorage.setItem('cart', JSON.stringify(cartData));
+        console.log('Quantité supplémentaire dans le panier.');
+        return;
+      }
+
+      cartData.push(selectedProducts);
+      localStorage.setItem('cart', JSON.stringify(cartData));
+      console.log('Le produit a été ajouté au panier');
+    } else {
+      let createLocalStorage = [];
+      createLocalStorage.push(selectedProducts);
+      localStorage.setItem('cart', JSON.stringify(createLocalStorage));
+      console.log('Le panier est vide, on ajoute le premier produit');
+    }
+  });
 };
 
 fetch(`http://localhost:3000/api/products/${productId}`)
